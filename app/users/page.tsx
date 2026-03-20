3560799ccdc36f217dba3ef39caa59283e63c4f7
Sparklingstadt <sparklingstadt@outlook.jp>
2026-03-20 19:28:28 +0900

    feat: add /users page.tsx

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,34 @@
-export default function Page() {
+import { getUsers } from "@/repositories/users"
+
+export default async function Page() {
+  const users = await getUsers()
+
   return (
     <div>
-      <h1>Users</h1>
+      <h1 className="text-2xl font-bold py-4">Users</h1>
+      <table className="w-full mx-auto border border-gray-300">
+        <thead>
+          <tr>
+            <th className="p-4 text-left font-normal text-gray-500 text-sm">ユーザー</th>
+            <th className="p-4 font-normal text-gray-500 text-sm">注文数</th>
+            <th className="p-4 font-normal text-gray-500 text-sm">ロール</th>
+            <th className="p-4 font-normal text-gray-500 text-sm">操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          { users.map(user => (
+            <tr key={user.id} className="border-t border-gray-300">
+              <td className="p-4">{user.firstName}</td>
+              <td className="p-4 text-center">N/A</td>
+              <td className="p-4 text-center">{user.role}</td>
+              <td className="p-4 text-center">
+                <button disabled>切り替え</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
     </div>
   )
 }
--- repositories/users/getUsers.ts
+++ repositories/users/getUsers.ts
@@ -1,9 +1,9 @@
 export async function getUsers() {
   const users = [
-    { id: 0, firstName: "Admin", lastName: "User" },
-    { id: 1, firstName: "Bill", lastName: "User" },
-    { id: 2, firstName: "Carter", lastName: "User" },
-    { id: 2, firstName: "Egol", lastName: "User" },
+    { id: 0, firstName: "Angelia", lastName: "User", role: "admin" },
+    { id: 1, firstName: "Bill", lastName: "User", role: "user" },
+    { id: 2, firstName: "Carter", lastName: "User", role: "user" },
+    { id: 3, firstName: "Egol", lastName: "User", role: "user" },
   ]
 
   return users
