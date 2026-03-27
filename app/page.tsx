971d183d8b1dd0653b638ddd8a8bfd9edf3caa6e
Sparklingstadt <sparklingstadt@outlook.jp>
2026-03-28 03:28:55 +0900

    feat: Add outline

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Page(){
   return (
     <div>
-      <h1>prd-ec-shop</h1>
+      <h1>Candy Rainとは？</h1>
+      <p>ECストアのデモ</p>
+      <p>開発の目的</p>
+      <p>使用しているもの</p>
+      <p>何ができるのか</p>
+      <p>どう設計したか</p>
+      <p>どう実装したか</p>
+      <p>学び</p>
+      <p>学びが今後どう活きるか</p>
     </div>
   )
 }
