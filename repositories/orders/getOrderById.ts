d57779eb74d95f5aef634bbec82211f5850c4865
Sparklingstadt <sparklingstadt@outlook.jp>
2026-03-31 18:33:47 +0900

    chore(repository/orderss): use prisma#findFirst method

--- repositories/orders/getOrderById.ts
+++ repositories/orders/getOrderById.ts
@@ -1,10 +1,17 @@
 import { cookies } from "next/headers";
 import { getOrders } from "./getOrders";
 import { redirect } from "next/navigation";
+import { prisma } from "@/lib/prisma";
 
 export async function getOrderById(id: number) {
   const userId = (await (cookies())).get("userId")?.value
   if(!userId) redirect("/signin")
 
-  return (await getOrders(parseInt(userId))).map(order => order.id === id)[0]
+  const order = await prisma.order.findFirst({
+    where: { userId: id }
+  })
+
+  if(!order) throw new Error("Order not found")
+
+  return order
 }
