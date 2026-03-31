23e7fd342cbbabd1aa5696d51bd376a899ce17c3
Sparklingstadt <sparklingstadt@outlook.jp>
2026-03-31 18:17:24 +0900

    bugfix(repositories/orders): require userId

--- repositories/orders/getOrderById.ts
+++ repositories/orders/getOrderById.ts
@@ -1,5 +1,10 @@
+import { cookies } from "next/headers";
 import { getOrders } from "./getOrders";
+import { redirect } from "next/navigation";
 
 export async function getOrderById(id: number) {
-  return (await getOrders()).map(order => order.id === id)[0]
+  const userId = (await (cookies())).get("userId")?.value
+  if(!userId) redirect("/signin")
+
+  return (await getOrders(parseInt(userId))).map(order => order.id === id)[0]
 }
