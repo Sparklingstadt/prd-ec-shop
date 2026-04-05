f7e2f7019c77212cc1af6d123daf8b0fad948b06
Sparklingstadt <sparklingstadt@outlook.jp>
2026-04-05 10:51:51 +0900

    feat(/cart): extract CartSummary.tsx

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link"
 import CartItemTable from "./CartItemTable"
 import PlaceOrderButton from "./PlaceOrderButton"
 import { requireUserId } from "@/lib/auth"
-import { getCartByUserId, getCartItemsByCartId, getCartItemsWithProductsByCartId } from "@/app/actions/actions"
+import { getCartByUserId, getCartItemsWithProductsByCartId } from "@/app/actions/actions"
+import { CartSummary } from "./CartSummary"
 
 export default async function Page(){
   const userId = await requireUserId()
@@ -17,29 +18,12 @@ export default async function Page(){
       <Link href="/products" className="text-sm underline">商品一覧へ戻る</Link>
       <p className="text-2xl font-bold py-4">買い物かご</p>
       <CartItemTable cart={cart} cartItems={cartItems} />
-      { cartItems.length > 0 && <div className="w-full mx-auto p-4 border border-t-0 border-gray-300">
-        <div className="flex justify-between mt-4">
-          <p>小計</p>
-          <p>¥{subTotalPrice}</p>
-        </div>
-        <div className="flex justify-between mt-4">
-          <p>送料</p>
-          <p>¥1000</p>
-        </div>
-        <div className="flex justify-between mt-4">
-          <p>消費税</p>
-          <p>¥300</p>
-        </div>
-        <div className="flex justify-between text-2xl mt-4">
-          <p>合計</p>
-          <p>¥{totalPrice}</p>
-        </div>
-      </div>
-      }
-      { cartItems.length === 0 && <div className="w-full mx-auto p-4 border border-t-0 border-gray-300">
-          <p>カートの中は空です</p>
-        </div>
-      }
+      <CartSummary
+        cartItems={cartItems}
+        subTotalPrice={subTotalPrice}
+        shippingFee={1000}
+        totalPrice={totalPrice}
+      />
       <div>
         <PlaceOrderButton userId={userId}/>
       </div>
