2be3e3e0bc339c6efe8263487119513e7da456e2
Sparklingstadt <sparklingstadt@outlook.jp>
2026-04-24 23:43:12 +0900

    feat(/signin): stylize sign-in form

--- app/(routes)/signin/SignInForm.tsx
+++ app/(routes)/signin/SignInForm.tsx
@@ -9,19 +9,19 @@ export function SignInForm() {
       }}
     >
       <div>
-        <label>
-          Email
-          <input name="email" type="email" className="border"/>
+        <label className="w-full flex justify-between mt-4">
+          <span>Email</span>
+          <input name="email" type="email" className="border p-2" placeholder="email@example.com" />
         </label>
       </div>
       <div>
-        <label>
-          Password
-          <input name="password" type="password" className="border"/>
+        <label className="w-full flex justify-between mt-4">
+          <span>Password</span>
+          <input name="password" type="password" className="border p-2" placeholder="*****" />
         </label>
       </div>
       <div className="mt-4">
-       <button className="border px-4 py-2">Sign In</button>
+       <button className="w-full bg-slate-800 text-white px-4 py-2">Sign In</button>
       </div>
     </form>
   )
