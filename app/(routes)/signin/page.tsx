4c9915830357c3480372f79bab552a0bca2121f1
Sparklingstadt <sparklingstadt@outlook.jp>
2026-04-25 00:02:37 +0900

    refactor(/signin): delete console.log

--- app/(routes)/signin/page.tsx
+++ app/(routes)/signin/page.tsx
@@ -1,11 +1,6 @@
 import { SignInForm } from "@/app/(routes)/signin/SignInForm"
-import { auth } from "@/auth"
 
 export default async function Page() {  
-  const session = await auth()
-
-  if(!session?.user) console.log("session not found")
-
   return (
     <div>
       <h1 className="text-2xl font-bold py-4">サインイン</h1>
