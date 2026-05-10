3188e2244ea0dcc305fbde7947af1efd77971e6e
Sparklingstadt <sparklingstadt@outlook.jp>
2026-05-10 23:08:34 +0900

    feat(/products/[id]): resize variants image size to 25 percent

--- app/(routes)/products/[id]/ProductImageView.tsx
+++ app/(routes)/products/[id]/ProductImageView.tsx
@@ -8,10 +8,10 @@ export default async function ProductImageView({ productId }: { productId: numbe
 
   return (
     <div>
-      <Image src={product.thumbnailImageUrl} alt="product img" width={1080} height={720}/>
+      <Image src={product.thumbnailImageUrl} alt="product img" width={1080} height={720} loading="eager" />
       <div className="flex">
         { variants.map((v: any) => (
-          <Image key={v.id} src={v.imageUrl} alt="product variant img" width={360} height={240} />
+          <Image key={v.id} src={v.imageUrl} alt="product variant img" width={360} height={240} style={{ width: "25%", height: "auto" }}/>
         ))}          
       </div>
     </div>
