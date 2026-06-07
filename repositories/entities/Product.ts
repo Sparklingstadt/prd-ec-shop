cc93902d2cf8b1bd6c5bc0ecbe65f7f57dee7eab
Sparklingstadt <sparklingstadt@outlook.jp>
2026-06-08 08:06:09 +0900

    refactor(entities/product): class Product

--- repositories/entities/Product.ts
+++ repositories/entities/Product.ts
@@ -1,18 +1,57 @@
-export type Product = {
-  id: number,
-  name: string,
-  category: string | null,
-  description: string,
-  thumbnailImageUrl: string,
+export class Product {
+  constructor(
+    public readonly id: number,
+    public readonly name: string,
+    public readonly category: string | null,
+    public readonly description: string,
+    public readonly thumbnailImageUrl: string,
+  ) {
+    if(!id) {
+      throw new Error("id is required!")
+    }
+    if(!name) {
+      throw new Error("name is required!")
+    }
+    if(!category) {
+      throw new Error("category is required!")
+    }
+    if(description) {
+      throw new Error("description is required!")
+    }
+    if(!thumbnailImageUrl) {
+      throw new Error("thumbnailImageUrl is required!")
+    }
+  }
 }
 
-export type ProductVariant = {
-  id: number,
-  name: string,
-  productId: number,
-  price: number,
-  stock: number,
-  imageUrl: string
+export class ProductVariant {
+  constructor(
+    public readonly id: number,
+    public readonly name: string,
+    public readonly productId: number,
+    public readonly price: number,
+    public readonly stock: number,
+    public readonly imageUrl: string
+  ) {
+    if(!id) {
+      throw new Error("id is required!")
+    }
+    if(!name) {
+      throw new Error("name is required!")
+    }
+    if(!productId) {
+      throw new Error("productId is required!")
+    }
+    if(!price) {
+      throw new Error("price is required!")
+    }
+    if(!stock) {
+      throw new Error("stock is required!")
+    }
+    if(!imageUrl) {
+      throw new Error("imageUrl is required!")
+    }
+  }
 }
 
 export type ProductWithVariant = Product & {
