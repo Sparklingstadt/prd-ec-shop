40a2876977000cd6521cbc9c47e0940ab0ebdd8f
Sparklingstadt <sparklingstadt@outlook.jp>
2026-06-08 09:50:33 +0900

    refactor(entities/user): class User

--- repositories/entities/User.ts
+++ repositories/entities/User.ts
@@ -1,5 +1,17 @@
-export type User = {
-  id: number,
-  firstName: string,
-  lastName: string
+export class User {
+  constructor(
+    public readonly id: number,
+    public readonly firstName: string,
+    public readonly lastName: string
+  ) {
+    if(!id) {
+      throw new Error("id is required!")
+    }
+    if(!firstName) {
+      throw new Error("firstName is required!")
+    }
+    if(!lastName) {
+      throw new Error("lastName is required!")
+    }
+  }
 }
