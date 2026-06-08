561fd49ab358cb9c5dad0ccc82510a5c2bfda20a
Sparklingstadt <sparklingstadt@outlook.jp>
2026-06-08 09:54:13 +0900

    refactor(entities/product): delete duplicate class

--- repositories/entities/Product.ts
+++ repositories/entities/Product.ts
@@ -1,3 +1,5 @@
+import { Variant } from "./Variant"
+
 export class Product {
   constructor(
     public readonly id: number,
@@ -24,36 +26,6 @@ export class Product {
   }
 }
 
-export class ProductVariant {
-  constructor(
-    public readonly id: number,
-    public readonly name: string,
-    public readonly productId: number,
-    public readonly price: number,
-    public readonly stock: number,
-    public readonly imageUrl: string
-  ) {
-    if(!id) {
-      throw new Error("id is required!")
-    }
-    if(!name) {
-      throw new Error("name is required!")
-    }
-    if(!productId) {
-      throw new Error("productId is required!")
-    }
-    if(!price) {
-      throw new Error("price is required!")
-    }
-    if(!stock) {
-      throw new Error("stock is required!")
-    }
-    if(!imageUrl) {
-      throw new Error("imageUrl is required!")
-    }
-  }
-}
-
 export type ProductWithVariant = Product & {
-  variants: ProductVariant[]
+  variants: Variant[]
 }
